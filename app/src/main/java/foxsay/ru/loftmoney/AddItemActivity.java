db44de0c54d7f046b5da7249476f6289535c7556
DimaChekashov <foxdima99@gmail.com>
2019-02-02 00:09:33 +0300

    created a check for two fields

--- app/src/main/java/foxsay/ru/loftmoney/AddItemActivity.java
+++ app/src/main/java/foxsay/ru/loftmoney/AddItemActivity.java
@@ -1,9 +1,6 @@
 package foxsay.ru.loftmoney;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,26 +17,13 @@ public class AddItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        name = (EditText) findViewById(R.id.add_item_name);
-        price = (EditText) findViewById(R.id.add_item_price);
-        addBtn = (Button) findViewById(R.id.add_item_button);
+        name = findViewById(R.id.add_item_name);
+        price = findViewById(R.id.add_item_price);
+        addBtn = findViewById(R.id.add_item_button);
 
-        name.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                addBtn.setEnabled(!TextUtils.isEmpty(s));
-            }
-        });
+        name.addTextChangedListener(new EditTextWatcher(price, addBtn));
+        price.addTextChangedListener(new EditTextWatcher(name, addBtn));
 
     }
 }
+
