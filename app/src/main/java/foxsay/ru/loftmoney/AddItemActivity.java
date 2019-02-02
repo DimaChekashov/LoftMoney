0117ce2b03bff27c7638051d2bcef82615309e7c
DimaChekashov <foxdima99@gmail.com>
2019-02-02 12:29:47 +0300

    delete EditTextWatcher and rewrite AddItemActivity

--- app/src/main/java/foxsay/ru/loftmoney/AddItemActivity.java
+++ app/src/main/java/foxsay/ru/loftmoney/AddItemActivity.java
@@ -1,8 +1,13 @@
 package foxsay.ru.loftmoney;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,9 +26,28 @@ public class AddItemActivity extends AppCompatActivity {
         price = findViewById(R.id.add_item_price);
         addBtn = findViewById(R.id.add_item_button);
 
-        name.addTextChangedListener(new EditTextWatcher(price, addBtn));
-        price.addTextChangedListener(new EditTextWatcher(name, addBtn));
+        name.addTextChangedListener(addItemWatcher);
+        price.addTextChangedListener(addItemWatcher);
 
     }
+
+    private TextWatcher addItemWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String nameEdit = name.getText().toString().trim();
+            String priceEdit = price.getText().toString().trim();
+
+            addBtn.setEnabled(!TextUtils.isEmpty(nameEdit) && !TextUtils.isEmpty(priceEdit));
+        }
+    };
 }
 
